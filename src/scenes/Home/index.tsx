3f179873b88a4f3df50c50c789d50f139130467c
Simon Degen <simon.degen@veni.no>
2020-06-15 16:58:46 +0200

    La til start Game

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -16,9 +16,6 @@ const Home = (props: any) => {
                 <Row>
                     <Button onClick={() => history.push("/joinGame")}>Join Game</Button>
                 </Row>
-                <Row>
-                    <Button onClick={() => history.push("/joinGame")}>Start Game</Button>
-                </Row>
             </Col>
         </Row>
     )
