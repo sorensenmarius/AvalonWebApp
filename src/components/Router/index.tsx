90e412dfb4e79129d2ed73fe4ed7d68ea80e5be6
Marius Sørensen <marius@netpower.no>
2020-07-25 10:36:48 +0200

    Ending screen and stuff

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -5,11 +5,149 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '../../scenes/Home';
 import HostGame from '../../scenes/Host/HostGame';
 import JoinGame from '../../scenes/Client/JoinGame';
-import PlayGame from '../../scenes/Client/PlayGame';
+import PlayGame from '../../scenes/Client/PlayGame'; 
+import HostGameEnded from '../../scenes/Host/HostGame/components/HostGameEnded';
 
 const Router = () => {
   return (
     <Switch>
+      <Route path='/test' render={(props: any) => <HostGameEnded 
+        game={{
+          "id": "c722e5f6-eeee-4903-05f4-08d83064811a",
+          "creationTime": new Date(),
+          "joinCode": "818068",
+          "players": [
+            {
+              "name": "sdfsfd",
+              "roleId": 2,
+              "roleInfo": "The Evil players are: sdfsdfsdf   Marius   ",
+              "roleName": "Merlin",
+              "isEvil": false,
+              "order": 0,
+              "id": "7b4cf115-598d-4953-eeeb-08d830649386"
+            },
+            {
+              "name": "sdfsdfsdf",
+              "roleId": 5,
+              "roleInfo": "Percival sees you as Merlin.|The evil players are: sdfsdfsdf & Marius",
+              "roleName": "Morgana",
+              "isEvil": true,
+              "order": 1,
+              "id": "501ac874-2bec-4e67-eeec-08d830649386"
+            },
+            {
+              "name": "Marius",
+              "roleId": 7,
+              "roleInfo": "If you figure out who Merlin is you win!|The evil players are: sdfsdfsdf & Marius",
+              "roleName": "Assassin",
+              "isEvil": true,
+              "order": 2,
+              "id": "086ed905-0007-48f5-eeed-08d830649386"
+            },
+            {
+              "name": "Marthe",
+              "roleId": 1,
+              "roleInfo": "You win if you can complete 3 Missions.",
+              "roleName": "Servant",
+              "isEvil": false,
+              "order": 3,
+              "id": "969b7484-4c9a-47e2-eeee-08d830649386"
+            },
+            {
+              "name": "Johanna",
+              "roleId": 3,
+              "roleInfo": "Merlin is: sdfsfd or sdfsdfsdf",
+              "roleName": "Percival",
+              "isEvil": false,
+              "order": 4,
+              "id": "b427c0cd-23d6-4b36-eeef-08d830649386"
+            }
+          ],
+          "status": -1,
+          "currentRound": {
+            "failedTeams": 0,
+            "currentTeam": [
+              {
+                "name": "Marthe",
+                "roleId": 1,
+                "roleInfo": "You win if you can complete 3 Missions.",
+                "roleName": "Servant",
+                "isEvil": false,
+                "order": 3,
+                "id": "969b7484-4c9a-47e2-eeee-08d830649386"
+              },
+              {
+                "name": "sdfsdfsdf",
+                "roleId": 5,
+                "roleInfo": "Percival sees you as Merlin.|The evil players are: sdfsdfsdf & Marius",
+                "roleName": "Morgana",
+                "isEvil": true,
+                "order": 1,
+                "id": "501ac874-2bec-4e67-eeec-08d830649386"
+              },
+              {
+                "name": "Marius",
+                "roleId": 7,
+                "roleInfo": "If you figure out who Merlin is you win!|The evil players are: sdfsdfsdf & Marius",
+                "roleName": "Assassin",
+                "isEvil": true,
+                "order": 2,
+                "id": "086ed905-0007-48f5-eeed-08d830649386"
+              }
+            ],
+            "status": 5,
+            "votesForTeam": 5,
+            "votesAgainstTeam": 0,
+            "missionVoteGood": 3,
+            "missionVoteBad": 0,
+            "requiredPlayers": 3,
+          },
+          "currentPlayer": {
+            "name": "Marthe",
+            "roleId": 1,
+            "roleInfo": "You win if you can complete 3 Missions.",
+            "roleName": "Servant",
+            "isEvil": false,
+            "order": 3,
+            "id": "969b7484-4c9a-47e2-eeee-08d830649386"
+          },
+          "pointsInnocent": 3,
+          "pointsEvil": 2,
+          "counter": 3,
+          "previousRounds": [
+            {
+              "failedTeams": 0,
+              "currentTeam": [
+                {
+                  "name": "Johanna",
+                  "roleId": 3,
+                  "roleInfo": "Merlin is: sdfsfd or sdfsdfsdf",
+                  "roleName": "Percival",
+                  "isEvil": false,
+                  "order": 4,
+                  "id": "b427c0cd-23d6-4b36-eeef-08d830649386"
+                }
+              ],
+              "status": 5,
+              "votesForTeam": 5,
+              "votesAgainstTeam": 0,
+              "missionVoteGood": 2,
+              "missionVoteBad": 0,
+              "requiredPlayers": 2,
+            },
+            {
+              "failedTeams": 0,
+              "currentTeam": [],
+              "status": 5,
+              "votesForTeam": 5,
+              "votesAgainstTeam": 0,
+              "missionVoteGood": 2,
+              "missionVoteBad": 0,
+              "requiredPlayers": 2,
+            }
+          ]
+        }} 
+      />} />
       <Route path='/host' render={(props: any) => <HostGame />} />
       <Route path='/play' render={(props: any) => <JoinGame />} />
       <Route path='/playGame' render={(props: any) => <PlayGame />} />
--- src/scenes/Host/HostGame/components/HostGameEnded/index.tsx
+++ src/scenes/Host/HostGame/components/HostGameEnded/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { observer } from 'mobx-react'
 import { Row, Col } from 'antd'
 import { Game } from '../../../../../models/Game/game'
 import { Player } from '../../../../../models/Players/player'
-
+import PlayerRole from '../../../../../models/Players/playerRole'
+import './index.less'
 interface HostGameEndedProps {
     game: Game
 }
@@ -11,64 +12,95 @@ interface HostGameEndedProps {
 const HostGameEnded = (props: HostGameEndedProps) => {
     const { game } = props
 
+    useEffect(() => {
+        console.log(game)
+    })
+
+    const assassinString = () => {
+        if(game.pointsEvil > 10) { // If Assassination
+            return(
+                <h2>{game.players.find(p => p.roleId === PlayerRole.Assassin)?.name} assassinated Merlin!</h2>
+            )
+        }
+        return
+    }
+
     return(
         <React.Fragment>
-            <Row>
-                <h1>Game has ended!</h1>
-            </Row>
-            <Row>
-                <Col>
-                    <Row>
-                        Good rounds
-                    </Row>
-                    <Row>
-                        {game.pointsInnocent}
-                    </Row>
-                </Col>
-                <Col>
-                    <Row>
-                        Evil rounds
-                    </Row>
-                    <Row>
-                        {game.pointsEvil}
-                    </Row>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Row><h1>Congrats to</h1></Row>
-                    <Row>
-                    {(() => {if(game.pointsInnocent > game.pointsEvil) {
-                            return(
-                                game.players.filter(p => !p.isEvil).map((p: Player) => (
-                                    <Col>
-                                        <Row>
-                                            <h3>{p.roleName}</h3>
-                                        </Row>
-                                        <Row>
-                                            <h2>{p.name}</h2>
-                                        </Row>
-                                    </Col>
-                                ))
-                            )
-                        } else {
-                            return(
-                                game.players.filter(p => p.isEvil).map((p: Player) => (
-                                    <Col>
-                                        <Row>
-                                            <h3>{p.roleName}</h3>
-                                        </Row>
-                                        <Row>
-                                            <h2>{p.name}</h2>
-                                        </Row>
-                                    </Col>
-                                ))
-                            )
-                        }
-                    })()}
-                    </Row>
-                </Col>
-            </Row>
+            <div className='gameEnded'>
+                <Row justify='center'>
+                    <h1>Game has ended!</h1>
+                </Row>
+                <Row
+                    justify='center'
+                    gutter={[32, 16]}
+                >
+                    <Col>
+                        <Row justify='center'>
+                            <h3>Good rounds</h3>
+                        </Row>
+                        <Row justify='center'>
+                            <h2>{game.pointsInnocent}</h2>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row justify='center'>
+                            <h3>Evil rounds</h3>
+                        </Row>
+                        <Row justify='center'>
+                            <h2>{game.pointsEvil > 10 ? game.pointsEvil - 100 : game.pointsEvil}</h2>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row
+                    justify='center'
+                >
+                    <Col>
+                        <Row justify='center'><h1>Congrats to</h1></Row>
+                        <Row
+                            justify='center'
+                            gutter={[24, 8]}
+                        >
+                        {(() => {if(game.pointsInnocent > game.pointsEvil) {
+                                return(
+                                    game.players.filter(p => !p.isEvil).map((p: Player) => (
+                                        <Col
+                                            key={p.id}
+                                        >
+                                            <Row justify='center'>
+                                                <h3>{p.roleName}</h3>
+                                            </Row>
+                                            <Row justify='center'>
+                                                <h2>{p.name}</h2>
+                                            </Row>
+                                        </Col>
+                                    ))
+                                )
+                            } else {
+                                return(
+                                    game.players.filter(p => p.isEvil).map((p: Player) => (
+                                        <Col
+                                            key={p.id}
+                                        >
+                                            <Row justify='center'>
+                                                <h3>{p.roleName}</h3>
+                                            </Row>
+                                            <Row justify='center'>
+                                                <h2>{p.name}</h2>
+                                            </Row>
+                                        </Col>
+                                    ))
+                                )
+                            }
+                        })()}
+                        </Row>
+                        <Row>
+                            {assassinString()}
+                        </Row>
+                    </Col>
+                </Row>
+            </div>
+            <div className='gameEndedBg' id={game.pointsInnocent > game.pointsEvil ? 'goodWin' : 'evilWin'} />
         </React.Fragment>
     )
 }
